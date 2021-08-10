624c06bceb714f260490f485d3ab6682ade23da1
marcelopajr <marcelo96jr@gmail.com>
2021-08-10 14:41:40 -0300

    Config server absolute url

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,14 +30,3 @@ export const getStaticProps = async () => {
     props: { articles },
   };
 };
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts?_limit=6`,
-//   );
-//   const articles = await res.json();
-
-//   return {
-//     props: { articles },
-//   };
-// };
